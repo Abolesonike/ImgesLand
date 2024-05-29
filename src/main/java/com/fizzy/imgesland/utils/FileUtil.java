91195c0a4a57f858f44d95831fe680229f30e1e0
renjinxian <jinxian.ren@hand-china.com>
2024-05-29 15:40:00 +0800

    重命名文件名

--- src/main/java/com/fizzy/imgesland/utils/FileUtil.java
+++ src/main/java/com/fizzy/imgesland/utils/FileUtil.java
@@ -4,6 +4,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -14,14 +16,16 @@ public class FileUtil {
     public static final String pathPattern = "image";
 
     //自己设置的目录
-    // public static final String fileDir = "D:\\MyProject\\images\\";
-    public static final String fileDir = "\\usr\\local\\image";
+    //public static final String fileDir = "D:\\MyProject\\images\\";
+    public static final String fileDir = "\\usr\\local\\image\\";
 
     public static String uploadFile(MultipartFile file) {
         String fileName = file.getOriginalFilename();
+        String fileType = fileName.substring(fileName.lastIndexOf("."));
+        String hashFileName = getFileName() + fileType;
 
-        String path = fileDir + getDayString();
-        String absolutePath = path + File.separator + fileName;
+        String path = fileDir;
+        String absolutePath = path + hashFileName;
 
         //首次需生成目录
         File folder = new File(path);
@@ -37,9 +41,33 @@ public class FileUtil {
         return absolutePath;
     }
 
-    private static String getDayString() {
+    private static String getFileName() {
         Date date = new Date();
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd", Locale.CHINA);
-        return df.format(date);
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.CHINA);
+        String format = df.format(date);
+        return md5(format);
     }
+
+    public static String toHexString(byte[] bytes) {
+        StringBuilder hexString = new StringBuilder();
+        for (byte b : bytes) {
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
+    public static String md5(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] messageDigest = md.digest(input.getBytes());
+            return toHexString(messageDigest);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
