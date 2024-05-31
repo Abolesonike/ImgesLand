cad8f4f4f623f8890ada273142348e5c29286d4c
renjinxian <jinxian.ren@hand-china.com>
2024-05-31 13:32:45 +0800

    跨域配置

--- src/main/java/com/fizzy/imgesland/config/WebMvcConfig.java
+++ src/main/java/com/fizzy/imgesland/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.fizzy.imgesland.config;
 import com.fizzy.imgesland.utils.FileUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,5 +15,20 @@ public class WebMvcConfig implements WebMvcConfigurer {
         //将匹配上/files/**虚拟路径的url映射到文件上传到服务器的目录，获取静态资源
         registry.addResourceHandler("/" + FileUtil.pathPattern + "/**").addResourceLocations("file:" + FileUtil.fileDir);
         WebMvcConfigurer.super.addResourceHandlers(registry);
+    }
+
+    @Configuration
+    public static class CorsConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    //是否发送Cookie
+                    .allowCredentials(true)
+                    //放行哪些原始域
+                    .allowedOriginPatterns("*")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+                    .allowedHeaders("*")
+                    .exposedHeaders("*");
+        }
     }
 }
